b5d22e9f97455979b68cd67aba4f7c9a79cb311c
texas <texas@DESKTOP-14P9BDC>
2017-02-17 01:51:39 -0600

    Agregar archivos de proyecto.

---
+++ ManejoElementos/WebForm1.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ManejoElementos
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void cambiar(object sender, EventArgs e)
+        {
+            Control f = Page.FindControl("form1");
+            Label l = (Label)f.FindControl("juan");
+            //TextBox t = (TextBox)f.FindControl("pedro");
+            l.Text = "Ola k ase?";
+        }
+    }
+}
